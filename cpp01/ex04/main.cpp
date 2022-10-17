edaf3382494e6d6463ad4a813dcc1e86813e141c
Rachida Moujan <rmoujan@student.1337.ma>
2022-10-17 17:45:09 +0100

    done with ex02/cpp01

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   testpointers.c                                     :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/08 18:05:42 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/10/16 21:42:56 by rmoujan          ###   ########.fr       */
+/*   Created: 2022/10/16 21:23:13 by rmoujan           #+#    #+#             */
+/*   Updated: 2022/10/16 21:24:40 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-int main()
+#include <iostream>
+
+int main(int argc, char *argv[])
 {
-
-    char *str = "hallo";
-
-    printf("%p\n", str);
-    printf("%p\n", &str);
-    // char *ptr = str;
-    // printf("str == %p\n", str);
-    // printf("str == %p\n", &str[0]);
-    // // printf("ptr == %p\n", ptr);
-    // // printf("str == %s\n", str);
-    // // printf("ptr == %s\n", ptr);
-    // printf("str == %p\n", &str);
-    // // printf("ptr == %p\n", &ptr);
-
-
     
-    return 0;
+    if (argc == 4)
+    {
+        
+    }
+    return (0);
 }
