7204cd971daec49c2b93d8699a30eedf37f6f785
Rachida Moujan <rmoujan@student.1337.ma>
2022-11-20 14:52:52 +0100

    ex04 in cpp01 I think I'm done with it

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 21:23:13 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/11/20 12:51:24 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/11/20 14:48:25 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,21 @@ int main(int argc, char *argv[]) {
     
     std::string line;
     std::string new_str;
-    
-    size_t found ;
-    size_t len;
-    
-    int i = 0;
-    int pos = 0;
 
-    len = strlen(argv[2]);
-    new_file.open("new_file");
+    size_t i = 0;
 
     if (argc == 4)
     {
         read_file.open(argv[1]);
         if (read_file)
         {
+            new_file.open("new_file");
             while (getline(read_file, line))
             {
                 i = 0;
                 while (line[i])
                 {
                     new_str = line.substr(i, strlen(argv[2]));
-                    std::cout<<" new str is |"<<new_str<<"| len is "<< strlen(argv[2])<<std::endl;
-                    std::cout<<new_str<<std::endl;
                     if (new_str.compare(argv[2]) == 0)
                     {
                         new_file<<argv[3];
@@ -56,12 +48,15 @@ int main(int argc, char *argv[]) {
                     if (i > line.length())
                         break;
                 }
-                printf("*******\n");
                 new_file<<std::endl;
             }   
         }
+        else
+        {
+            std::cout<< "\033[31m ERROR OCCURS WHEN OEPNING THE FILE\033[0m"<<std::endl;
+        }
         return (0);
     }
-    // std::cout << "\033[31m UR ENTERED MORE/LESS PARAMETERS EXPECTED \033[0m"<<std::endl;
+    std::cout << "\033[31m UR ENTERED MORE/LESS PARAMETERS EXPECTED \033[0m"<<std::endl;
     return (0);
 }
