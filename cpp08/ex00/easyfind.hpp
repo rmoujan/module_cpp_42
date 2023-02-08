471419992c716c524d4ed1a590246f6337d19159
Rachida Moujan <rmoujan@student.1337.ma>
2023-02-08 16:20:19 +0100

    starting first ex in cpp08

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 16:14:15 by rmoujan           #+#    #+#             */
+/*   Updated: 2023/02/08 16:16:03 by rmoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+
+easyfind(T arg, int value)
+{
+	
+}
+
+
+#endif
