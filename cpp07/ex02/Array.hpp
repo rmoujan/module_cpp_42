ac745ab20718e478afdde056a3d30145c5b0681a
Rachida Moujan <rmoujan@student.1337.ma>
2023-02-08 14:43:25 +0100

    starting ex02 day 07

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 22:36:41 by rmoujan           #+#    #+#             */
-/*   Updated: 2023/02/07 23:15:04 by rmoujan          ###   ########.fr       */
+/*   Updated: 2023/02/08 14:38:22 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,58 +17,80 @@
 template <class T>
 class Array {
     T *data;
+	int sizearr;
 
 
    public:
 	Array()
 	{
 		data = new T();
+		sizearr = 0;
+		std::cout <<"default const "<<std::endl;
 	
 	}
 	Array(unsigned int n)
 	{
-		data = new T[n];
-		for(unsigned int i = 0; i < n; i++)
+		data = new T[n + 1];
+		unsigned int i;
+		for(i = 0; i < n ; i++)
 		{
 			data[i] = 5;
 		}
-
+		data[i] = '\0';
+		sizearr = n;
+		std::cout <<" const by param "<<std::endl;
 	}
 	Array(const Array & ref)
 	{
 		*this= ref;
+		std::cout <<"copy const"<<std::endl;
 	}
 	
 	Array& operator=(const Array & ref)
 	{
 		// this->size =  ref.size;
-		this->data = new T[ref.data.length()];
-		
-		for(int i = 0; i < ref.data.length(); i++)
+		this->data = new T[ref.sizearr];
+		int i = 0;
+		while (ref.data[i])
 		{
 			data[i] = ref.data[i];
+			i++;
 		}
+		data[i] = '\0';
+		std::cout <<"copy assignement "<<std::endl;
+		sizearr = ref.sizearr;
 		return (*this);
 	}
 	
 
 	void output()
 	{
-		for (int i = 0; i <5; i++)
+		int i = 0;
+		while (data[i] != '\0')
 		{
 			std::cout <<data[i]<<std::endl;
+			i++;
 		}
 	}
-	// int size()
-	// {
-	// 	int count = 0;
-	// 	for (int i = 0; i <data.size(); i++)
-	// 	{
-	// 		count++;
-	// 	}
-	// 	return count;
-	// }
-	
+	int size()
+	{
+		return (sizearr);
+	}
+	void getElement(int index)
+	{
+		std::cout <<sizearr<<std::endl;
+		try{
+			if (index < 0 || index >= sizearr )
+			{
+				throw (-1);
+			}
+			std::cout <<" ELEMENT IS = "<<data[index]<<std::endl;
+		}
+		catch (int code)
+		{
+			std::cout <<"OUT OF RANGE "<<std::endl;
+		}
+	}
 };
 
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 22:36:43 by rmoujan           #+#    #+#             */
-/*   Updated: 2023/02/07 23:12:47 by rmoujan          ###   ########.fr       */
+/*   Updated: 2023/02/08 14:43:06 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,20 @@
 
 int main()
 {
-	Array <int> test(5);
+	Array <int> *test = new Array<int>(4);
 	// std::cout <<"test is  === " <<test.size()<<std::endl;
-	test.output();
+	test->output();
+	std::cout <<"--------------------------"<<std::endl;
+	Array <int> *copy = new Array<int>();
+	*copy = *test;
+	copy->output();
+	copy->getElement(4);
+	std::cout <<"size is  === "<<copy->size()<<std::endl;
+	std::cout <<"********************"<<std::endl;
+	// Array<int> *save(*copy);
+	save->output();
+	delete test;
+	delete copy;
+	// delete save;
 	return (0);
 }
