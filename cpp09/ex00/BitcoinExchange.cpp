cca40f2fbad6b8757a44c6942436a215a52b216c
rmoujan@student.1337.ma <rmoujan@student.1337.ma>
2023-03-19 22:22:31 +0000

    check value

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -47,8 +47,67 @@ int BitcoinExchange :: check_format_line(std::string data)
 	return (1);	
 }
 
+int check_year(std::string year)
+{
+    int number = 0;
+
+	if (year.lenght() != 4)
+		return (0);
+    stringstream object;
+    object << year;
+    object >> number;
+
+	if (number < 2009 || number > 2022)
+		return (0);
+    return (1);
+}
+
+int check_month(std::string month)
+{
+    int number = 0;
+
+	if (month.lenght() != 2)
+		return (0);
+    stringstream object;
+    object << year;
+    object >> number;
+	if (number < 1 || number > 12)
+		return (0);
+    return (1);
+}
+
+int check_day(std::string day, std::string month)
+{
+    int d = 0;
+	int m = 0;
+	if (day.lenght() != 2)
+		return (0);
+
+    stringstream object_d;
+    object_d << day;
+    object_d >> d;
+
+	stringstream object_m;
+    object_m << month;
+    object_m >> m;
+
+	if (m == 2)
+	{
+		if (d < 1 || d > 28)
+			return (0);
+	}
+	else
+		if (d < 1 || d > 31)
+			return (0);
+    return (1);
+}
+
+
 int check_first(std::string word)
 {
+	std::string result;
+	std::string month;
+	int count = 0;
 	//check is that last char is a space
 	if (word[word.length() - 1] != ' ')
 		return (0);
@@ -56,7 +115,23 @@ int check_first(std::string word)
 	else if(!isdigit(word[word.length() - 2]))
 		return (0);
 	//split the date by - and count the chars, must be 3 :
-	
+	std::stringstream str(word);
+	while (getline(str, result, '-'))
+	{
+		if (count == 0)
+			check_year(result);
+		else if (count == 1)
+		{
+			check_month(result);
+			month = result;
+		}
+		else if(count == 2)
+			check_day(result, month);
+		
+		count++;
+	}
+	if (count != 2)
+		return (0);
 	return (1);
 }
 
@@ -69,9 +144,9 @@ int BitcoinExchange :: check_date(std::string data)
 	if (!isdigit(data[0]))
 		return (0);
 	std::stringstream str(data);
-	getline(str, word, '|')
+	getline(str, word, '|');
 	check_first(word);
-	getline(str, word, '|')
+	getline(str, word, '|');
 	check_second(word);
 	return (1);	
 }
@@ -85,15 +160,11 @@ void BitcoinExchange :: output_data(std::string filee) {
 	infile.open(filee);
 	while ((infile >> data)) {
 		check_format_line(data); //"date | value"
-		check_date(data);		 //"YEAR-MONTH-DAY", YEAR MUST ENTER 2009 AND 2023 AND CHECK MONTH AND DAY
+		check_date(data);		 //"YEAR-MONTH-DAY", YEAR MUST ENTER 2009 AND 202 AND CHECK MONTH AND DAY
 		check_number();			 // GREATHER THAN 1000 OR LESS THAN 0
 		std::stringstream str(data);
 		std::getline(str, key, ',');
 		std::getline(str, value, ',');
-		// std::cout << "key is " << key << " and value is |" << value << "|" << std::endl;
-		// if (key.empty() || value.empty()) {
-		// 	ft_errno(2);
-		// }
 		base.insert(std::pair<std::string, std::string>(key, value));
 	}
 }
--- cpp09/ex00/test.cpp
+++ cpp09/ex00/test.cpp
@@ -1,56 +1,83 @@
-#include <fstream>
+// #include <fstream>
+// #include <iostream>
+// #include <sstream>
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// int main() {
+// 	// string fname;
+// 	// cout << "Enter the file name: ";
+// 	// cin >> fname;
+// 	// string line, word;
+
+// 	// fstream file(fname, ios::in);
+// 	// if (file.is_open()) {
+// 	// 	while (getline(file, line)) {
+
+// 	// 		stringstream str(line);
+// 	// 		// cout << "Line is |" << line <<"|"<<endl;
+// 	// 			// std::cout <<getline(str, word, '|');
+// 	// 		getline(str, word, '|');
+// 	// 		cout << "  word is |" << word <<"|"<<endl;
+// 	// 		getline(str, word, '|');
+// 	// 		cout << "  word is |" << word <<"|"<<endl;
+// 	// 			// cout << "  str is " << line << std::endl;
+			
+// 	// 		cout << endl;
+// 	// 	}
+// 	// }
+// 	std::string word = "2011-01-03-78";
+// 	std::stringstream str(word);
+// 	std::string result;
+// 	int count = 0;
+// 	while (getline(str, result, '-'))
+// 	{
+// 		count++;
+// 	}
+	
+// 	cout<<"str is "<<count;
+// //   std::string food = "hallo world 2023";
+// //   std::cout << food.find("o")<<endl;
+// // //   if (food.find("|") == 18446744073709551615)
+// // if (food.find("h") == string::npos)
+// // 		cout <<" NOT FOUND "<<endl;
+// 	// std::string s = "Year-Month-Day-";
+//   	// size_t n = std::count(s.begin(), s.end(), '-');
+// 	// cout<<n;
+
+// 	// cout << isdigit('q');
+// 		// cout <<"ok";
+
+
+
+// 	return 0;
+// }
+
+
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
-
+#include <string>
+#include <iostream>
+#include <sstream>
 using namespace std;
-
 int main() {
-	// string fname;
-	// cout << "Enter the file name: ";
-	// cin >> fname;
-	// string line, word;
-
-	// fstream file(fname, ios::in);
-	// if (file.is_open()) {
-	// 	while (getline(file, line)) {
-
-	// 		stringstream str(line);
-	// 		// cout << "Line is |" << line <<"|"<<endl;
-	// 			// std::cout <<getline(str, word, '|');
-	// 		getline(str, word, '|');
-	// 		cout << "  word is |" << word <<"|"<<endl;
-	// 		getline(str, word, '|');
-	// 		cout << "  word is |" << word <<"|"<<endl;
-	// 			// cout << "  str is " << line << std::endl;
-			
-	// 		cout << endl;
-	// 	}
-	// }
-	std::string word = "2011-01-03-78";
-	std::stringstream str(word);
-	std::string result;
-	int count = 0;
-	while (getline(str, result, '-'))
-	{
-		count++;
-	}
-	
-	cout<<"str is "<<count;
-//   std::string food = "hallo world 2023";
-//   std::cout << food.find("o")<<endl;
-// //   if (food.find("|") == 18446744073709551615)
-// if (food.find("h") == string::npos)
-// 		cout <<" NOT FOUND "<<endl;
-	// std::string s = "Year-Month-Day-";
-  	// size_t n = std::count(s.begin(), s.end(), '-');
-	// cout<<n;
-
-	// cout << isdigit('q');
-		// cout <<"ok";
 
 
+	std::string year = "09";
+	// std::cout <<year.length();
 
-	return 0;
+    int number = 0;
+
+	// if (year.lenght() != 4)
+	// 	return (0);
+    stringstream object;
+    object << year;
+    object >> number;
+	cout <<"number is "<<number<<endl;
+	if (number < 1 || number > 12)
+		cout <<number;
+
 }
